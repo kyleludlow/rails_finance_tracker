1f6c02dd7a4305d69391a38745a0b0cdb39907a0
Kyle <kyleludlow@gmail.com>
2016-12-17 01:57:12 -0800

    adds devise and bootstrap for authentication and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # any action from UI needs authenticated user
+  before_action :authenticate_user!
 end
